d46b15ea27041807ada67bd9b68ef109a37251f0
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-01-30 16:02:55 -0300

    adicionando logo no header

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,25 @@
+import React from "react";
+import { Image } from "react-native";
+
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
 import Feed from "../screens/Feed";
 
-const MainNavigator = createStackNavigator({
-  Feed
-});
+import camera from "../../assets/camera.png";
+import logo from "../../assets/logo.png";
+import igtv from "../../assets/igtv.png";
+import send from "../../assets/send.png";
+
+const MainNavigator = createStackNavigator(
+  {
+    Feed
+  },
+  {
+    defaultNavigationOptions: {
+      headerTitle: <Image source={logo} />
+    }
+  }
+);
 
 export default createAppContainer(MainNavigator);
